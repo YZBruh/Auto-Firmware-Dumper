3ed80b251f84a6680abdc0cd58716368d62fc3d9
YZBruh <131263805+YZBruh@users.noreply.github.com>
2023-12-03 21:11:12 +0300

    Try fix error

--- scripts/control_all_dt.sh
+++ scripts/control_all_dt.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/bash
 # Check all trees
 # By @YZBruh
-echo "Copying TWRP and LineageOS tree"
+echo "Checking TWRP and LineageOS tree"
 # Checking and copying los dt
 cd /home/Auto-Dumper/DumprX/out && basename lineage-device-tree
 exit_code=$?
 if [ $exit_code -eq 0 ]; then
-    cp -r lineage-device-tree /home/Auto-Dumper
     unset exit_code
 else
     echo "Lineage device tree not found!."
@@ -17,7 +16,6 @@ fi
 basename twrp-device-tree
 exit_code=$?
 if [ $exit_code -eq 0 ]; then
-    cp -r twrp-device-tree /home/Auto-Dumper
     unset exit_code
 else
     echo "TWRP device tree not found!"
